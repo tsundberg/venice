ce3185fe464e340ceb63333ca899b6811df25a42
Thomas Sundberg <thomas.sundberg@arbetsformedlingen.se>
2017-01-30 14:20:09 +0100

    Added Bootstrap and JQuery

--- src/main/java/se/arbetsformedlingen/venice/Main.java
+++ src/main/java/se/arbetsformedlingen/venice/Main.java
@@ -4,9 +4,11 @@ import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
 import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 public class Main {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         get("/", (request, response) -> new ModelAndView(null, "index.mustache"), new MustacheTemplateEngine());
     }
 }
