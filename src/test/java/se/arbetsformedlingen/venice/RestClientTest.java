4b89390236e4bde1e181a028bcab0c50fa6de399
Thomas Sundberg <thomas.sundberg@arbetsformedlingen.se>
2017-02-09 12:12:50 +0100

    Spike for calling probes

--- src/test/java/se/arbetsformedlingen/venice/RestClientTest.java
+++ src/test/java/se/arbetsformedlingen/venice/RestClientTest.java
@@ -3,29 +3,46 @@ package se.arbetsformedlingen.venice;
 
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RestClientTest {
 
     @Test
-    public void rest_client() throws InterruptedException, IOException {
-        String uri = "https://jsonplaceholder.typicode.com/posts/1";
+    @Ignore
+    public void rest_client() throws Exception {
+        String uri = "http://gfr.u1/wildfly05/jolokia/read/af-probe:probe=UgkForetagProbe/";
 
-        String res = Request.Get(uri)
-                .connectTimeout(1000)
-                .socketTimeout(1000)
-                .execute().returnContent().asString();
-
-        System.out.println(res);
-
+        String user = "unset";
+        String password = "secret";
         Executor executor = Executor.newInstance()
-                .auth("username", "password");
+                .auth(user, password);
 
-        res = executor.execute(Request.Get(uri))
+        String res = executor.execute(Request.Get(uri))
                 .returnContent().asString();
 
-        System.out.println(res);
+        CompletableFuture<Object> foo = CompletableFuture.supplyAsync(() -> {
+            String r = "Failure";
+            try {
+                r = executor.execute(Request.Get(uri))
+                        .returnContent().asString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return r;
+        });
+
+        while (!foo.isDone()) {
+            System.out.print(".");
+            Thread.sleep(50);
+        }
+
+        assertThat(foo.get()).isEqualTo("Hello");
     }
+
 }
