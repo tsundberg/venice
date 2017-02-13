582c763b09af84f201dc4b0a674c204d38b55cc5
Thomas Sundberg <thomas.sundberg@arbetsformedlingen.se>
2017-02-13 11:30:24 +0100

    Actual check probe, connect to a host and expect a response

---
+++ src/integration-test/java/se/arbetsformedlingen/venice/probe/CheckProbeTest.java
@@ -0,0 +1,21 @@
+package se.arbetsformedlingen.venice.probe;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CheckProbeTest {
+
+    @Test
+    public void check_gfr_u1() {
+        Host host = new Host("L7700649.u1.local");
+        Application application = new Application("gfr");
+        CheckProbe probe = new CheckProbe(host, application);
+
+        ProbeResponse actual = probe.get();
+
+        assertThat(actual.getHost()).isEqualTo(host);
+        assertThat(actual.getApplication()).isEqualTo(application);
+    }
+
+}
