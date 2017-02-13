56db8b7571010ddce495decde8f9fd256a294220
Thomas Sundberg <thomas.sundberg@arbetsformedlingen.se>
2017-02-13 15:33:18 +0100

    Use String versions everywhere

--- src/integration-test/java/se/arbetsformedlingen/venice/probe/CheckProbeTest.java
+++ src/integration-test/java/se/arbetsformedlingen/venice/probe/CheckProbeTest.java
@@ -10,14 +10,16 @@ public class CheckProbeTest {
 
     @Test
     public void check_gfr_u1() {
-        Host host = new Host("L7700649.u1.local");
-        Application application = new Application("gfr");
+        String hostName = "L7700649.u1.local";
+        Host host = new Host(hostName);
+        String applicationName = "gfr";
+        Application application = new Application(applicationName);
         CheckProbe probe = new CheckProbe(host, application);
 
         ProbeResponse actual = probe.get();
 
-        assertThat(actual.getHost()).isEqualTo(host);
-        assertThat(actual.getApplication()).isEqualTo(application);
+        assertThat(actual.getHostName()).isEqualTo(hostName);
+        assertThat(actual.getApplicationName()).isEqualTo(applicationName);
     }
 
 }
