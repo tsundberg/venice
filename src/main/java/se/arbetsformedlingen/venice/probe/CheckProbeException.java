9f76ae214098ad86951a33d33aa7aee5018cd098
Thomas Sundberg <thomas.sundberg@arbetsformedlingen.se>
2017-02-13 11:30:54 +0100

    Hide use our own exceptions when failing

---
+++ src/main/java/se/arbetsformedlingen/venice/probe/CheckProbeException.java
@@ -0,0 +1,7 @@
+package se.arbetsformedlingen.venice.probe;
+
+class CheckProbeException extends RuntimeException {
+    CheckProbeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
