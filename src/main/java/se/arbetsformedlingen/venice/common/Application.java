9698fef09342401be0634d8d69f8d3c5ae2c814e
Thomas Sundberg <thomas.sundberg@arbetsformedlingen.se>
2017-02-13 16:51:01 +0100

    Add environment to the application

--- src/main/java/se/arbetsformedlingen/venice/common/Application.java
+++ src/main/java/se/arbetsformedlingen/venice/common/Application.java
@@ -1,16 +1,28 @@
 package se.arbetsformedlingen.venice.common;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Application {
     private String application;
+    private List<Environment> environments;
 
     public Application(String application) {
         this.application = application;
+        this.environments = new ArrayList<>();
     }
 
     public String getApplicationName() {
         return application;
+    }
+
+    public void addEnvironment(Environment env) {
+        this.environments.add(env);
+    }
+
+    public List<Environment> getEnvironments() {
+        return environments;
     }
 
     @Override
---
+++ src/main/java/se/arbetsformedlingen/venice/common/Environment.java
@@ -0,0 +1,26 @@
+package se.arbetsformedlingen.venice.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Environment {
+
+    private String name;
+    private List<Host> hosts;
+
+    public Environment(String name) {
+        this.name = name;
+        this.hosts = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addHost(Host host) {
+        hosts.add(host);
+    }
+    public List<Host> getHosts() {
+        return hosts;
+    }
+}
