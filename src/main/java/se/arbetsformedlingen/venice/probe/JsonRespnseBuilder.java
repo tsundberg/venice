4351f3854a17ad3f033a76399c3727a560f1b65d
Thomas Sundberg <tsu@kth.se>
2017-03-08 08:15:13 +0100

    Refactoring building json

--- src/main/java/se/arbetsformedlingen/venice/probe/JsonRespnseBuilder.java
+++ src/main/java/se/arbetsformedlingen/venice/probe/JsonRespnseBuilder.java
@@ -9,68 +9,83 @@ import se.arbetsformedlingen.venice.common.Server;
 import java.util.LinkedList;
 import java.util.List;
 
-public class JsonRespnseBuilder {
+class JsonRespnseBuilder {
     private LatestProbeStatuses statuses;
 
-    public JsonRespnseBuilder(LatestProbeStatuses statuses) {
+    JsonRespnseBuilder(LatestProbeStatuses statuses) {
         this.statuses = statuses;
     }
 
-    public String build(List<Server> servers) {
-        List<Application> apps = new LinkedList<>();
-        servers.forEach(srv -> {
-            Application application = srv.getApplication();
-            if (!apps.contains(application)) {
-                apps.add(application);
-            }
-        });
-
-        List<Environment> envs = new LinkedList<>();
-        servers.forEach(srv -> {
-            Environment environment = srv.getEnvironment();
-            if (!envs.contains(environment)) {
-                envs.add(environment);
-            }
-        });
+    String build(List<Server> servers) {
+        List<Application> applications = getApplications(servers);
+        List<Environment> environments = getEnvironments(servers);
 
         JSONArray response = new JSONArray();
-        for (Application app : apps) {
-            JSONObject appObj = new JSONObject();
-            appObj.put("name", app.getName());
+        for (Application application : applications) {
+            JSONObject applicationJson = addApplicationName(application.getName());
 
             JSONArray envList = new JSONArray();
-            for (Environment env : envs) {
-
-                JSONObject envObj = new JSONObject();
-                envObj.put("name", env.getName());
+            for (Environment environment : environments) {
+                JSONObject environmentJson = addApplicationName(environment.getName());
 
                 JSONObject serverList = new JSONObject();
                 for (Server server : servers) {
-                    if (!app.equals(server.getApplication())) {
-                        continue;
-                    }
-                    if (!env.equals(server.getEnvironment())) {
-                        continue;
-                    }
-
-                    ProbeResponse probeRes = statuses.getStatus(server);
-
-                    JSONObject serverObj = new JSONObject();
-                    serverObj.put("status", probeRes.getStatus());
-                    serverObj.put("version", probeRes.getVersion());
-
-                    serverList.put(server.getHost().toString(), serverObj);
+                    addServer(application, environment, serverList, server);
                 }
 
-                envObj.put("servers", serverList);
-
-                envList.put(envObj);
+                environmentJson.put("servers", serverList);
+                envList.put(environmentJson);
             }
-            appObj.put("environments", envList);
+            applicationJson.put("environments", envList);
 
-            response.put(appObj);
+            response.put(applicationJson);
         }
 
         return response.toString();
+    }
+
+    private JSONObject addApplicationName(String name) {
+        JSONObject appObj = new JSONObject();
+        appObj.put("name", name);
+        return appObj;
+    }
+
+    private void addServer(Application app, Environment env, JSONObject serverList, Server server) {
+        if (!app.equals(server.getApplication())) {
+            return;
+        }
+        if (!env.equals(server.getEnvironment())) {
+            return;
+        }
+
+        ProbeResponse probeRes = statuses.getStatus(server);
+
+        JSONObject appObj = new JSONObject();
+        appObj.put("status", probeRes.getStatus());
+        appObj.put("version", probeRes.getVersion());
+
+        serverList.put(server.getHost().toString(), appObj);
+    }
+
+    private List<Application> getApplications(List<Server> servers) {
+        List<Application> applications = new LinkedList<>();
+        servers.forEach(srv -> {
+            Application application = srv.getApplication();
+            if (!applications.contains(application)) {
+                applications.add(application);
+            }
+        });
+        return applications;
+    }
+
+    private List<Environment> getEnvironments(List<Server> servers) {
+        List<Environment> environments = new LinkedList<>();
+        servers.forEach(srv -> {
+            Environment environment = srv.getEnvironment();
+            if (!environments.contains(environment)) {
+                environments.add(environment);
+            }
+        });
+        return environments;
     }
 }
