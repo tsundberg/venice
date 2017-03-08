f3b91d8e239a0f9156376c90e427bee00f375fb8
Thomas Sundberg <tsu@kth.se>
2017-03-08 09:19:58 +0100

    Verify that we can handle broken Json from TPJAdmin

--- src/test/java/se/arbetsformedlingen/venice/tpjadmin/TPJAdminResponseParserTest.java
+++ src/test/java/se/arbetsformedlingen/venice/tpjadmin/TPJAdminResponseParserTest.java
@@ -29,4 +29,23 @@ public class TPJAdminResponseParserTest {
         assertThat(actual).containsExactlyInAnyOrder(geo1, geo2, geo3);
     }
 
+    @Test
+    public void skip_parsing_response_with_broken_instance_number() {
+        Application application = new Application("geo");
+        Environment environment = new Environment("PROD");
+        Host host1 = new Host("l7700746.wpa.ams.se");
+        Host host3 = new Host("l7700770.wpa.ams.se");
+        Port port = new Port("8180");
+
+        ApplicationServer geo1 = new ApplicationServer(application, environment, host1, port);
+        ApplicationServer geo3 = new ApplicationServer(application, environment, host3, port);
+
+        String instanceNumber = "broken instance number";
+        String jsonSample = "{\"l7700770.wpa.ams.se\":1, \"l7700747.wpa.ams.se\": " + instanceNumber + ", \"l7700746.wpa.ams.se\":1}";
+
+        List<ApplicationServer> actual = TPJAdminResponseParser.parse(application, environment, jsonSample);
+
+        assertThat(actual).containsExactlyInAnyOrder(geo1, geo3);
+    }
+
 }
