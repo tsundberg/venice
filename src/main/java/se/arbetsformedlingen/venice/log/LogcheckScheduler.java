8da1f30d68b8c32fb7e067e411bafbb11e04f42c
Thomas Sundberg <thomas.sundberg@arbetsformedlingen.se>
2017-03-13 11:44:27 +0100

    Moved the runner to an internal class instead, the only reason it exists is because the Java scheduler implementation needs a runnable for starting scheduling

--- src/main/java/se/arbetsformedlingen/venice/log/LogcheckScheduler.java
+++ src/main/java/se/arbetsformedlingen/venice/log/LogcheckScheduler.java
@@ -4,24 +4,39 @@ import se.arbetsformedlingen.venice.common.Scheduler;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class LogcheckScheduler implements Scheduler {
-    private LatestLogs latestProbeStatuses = new LatestLogs();
-    private List<LogChecker> checkers = new LinkedList<>();
+    private LatestLogs latestLogs = new LatestLogs();
+    private List<Checker> checkers = new LinkedList<>();
 
     public LogcheckScheduler() {
-        checkers.add(new LogChecker(new CheckLogs()));
+        checkers.add(new Checker(new CheckLogs()));
     }
 
     @Override
     public void startChecking(int period, TimeUnit unit) {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(11);
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(16);
 
-        for (LogChecker checker : checkers) {
+        for (Checker checker : checkers) {
             scheduler.scheduleAtFixedRate(checker, 1, period, unit);
+        }
+    }
+
+    private class Checker implements Runnable {
+        private CheckLogs checkLogs;
+
+        private Checker(CheckLogs checkLogs) {
+            this.checkLogs = checkLogs;
+        }
+
+        @Override
+        public void run() {
+            CompletableFuture.supplyAsync(checkLogs)
+                    .thenAccept(result -> latestLogs.addLog(result));
         }
     }
 }
